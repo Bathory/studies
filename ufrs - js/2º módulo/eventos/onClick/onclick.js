c176c7111c498fb360c8ae637c30ae706bb6dc00
Leticia <neves@tutamail.com>
2022-08-29 11:18:40 -0300

    old studies in js

---
+++ ufrs - js/2º módulo/eventos/onClick/onclick.js
@@ -0,0 +1,9 @@
+function umClique(){
+    document.getElementById("imagem").src="pfto.jpg";
+    document.getElementById("log").innerHTML += "aparece o homem mais pfto do mundo 😳<br>";
+}
+function doisCliques(){
+    document.getElementById("imagem").src="sebastian.png";
+    document.getElementById("log").innerHTML += "volta para o sebastian 😠<br>";
+}
+
